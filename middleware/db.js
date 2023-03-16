2f36a412ef39bff8d646e25d77960bda0ab9cea7
Mian Zohaib Asghar <zohaibmehar123456@gmail.com>
2023-03-16 22:33:27 +0500

    mongo atlas connected

--- middleware/db.js
+++ middleware/db.js
@@ -5,9 +5,13 @@ const connectDB = (handler) => async (req, res) => {
     mongoose.set("strictQuery", false);
     return handler(req, res);
   }
-  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  await mongoose.connect("mongodb+srv://zohaib:dVqrzGs1eC5YVCTl@cluster0.ozggqrs.mongodb.net/test");
   console.log("Mongo Connected");
   return handler(req, res);
 };
 
 export default connectDB;
+
+//  MongoDB atlas cred
+// dVqrzGs1eC5YVCTl    zohaib         password
+// QiCga2EhjyieDgfCk7pubTBtcCT3EClVKRJXPiZXtVyPLPcxqTD7YCwMW8bA62ge   API
